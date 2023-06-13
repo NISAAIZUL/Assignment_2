d96622d1e3b25aaa6de9653dfcd23e30c5117f10
NISAAIZUL <nisaaizul18@gmail.com>
2023-06-14 01:55:26 +0800

    try commit

---
+++ app/src/main/java/com/example/assignment1/AboutActivity.java
@@ -0,0 +1,20 @@
+package com.example.assignment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+;
+
+
+public class AboutActivity extends AppCompatActivity {
+    private Object view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+
+
+        }
+    }
+
